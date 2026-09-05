5fa33cec56d84347ae30542dd8466ecae6aaeda5
JeancarlosG97 <MrJeancarlosG@Gmail.com>
2026-09-05 13:38:47 -0400

    allow deployed frontend origin

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ from main import (
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://resume-analyzer-ui-rg64.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
